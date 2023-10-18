9bd7052ab410e6ae9aab024700b2064e550d3379
Christian Laußmann <christian@laussmann.me>
2023-10-18 16:01:40 +0200

    ADD: Tests

--- tests/properties/test_metrics.py
+++ tests/properties/test_metrics.py
@@ -6,7 +6,7 @@ from unittest import TestCase
 
 class TestAnalysis(TestCase):
 
-    def test_pairwise_scores(self):
+    def test_pairwise_scores_strict(self):
         instance = OrdinalInstance()
         orders = [((0,), (1,), (2,)),
                   ((0,), (1,), (2,)),
@@ -16,6 +16,17 @@ class TestAnalysis(TestCase):
         assert scores[0][1] == 2
         assert scores[1][0] == 1
         assert scores[1][2] == 2
+    
+    def test_pairwise_scores_tied(self):
+        instance = OrdinalInstance()
+        orders = [((0,), (1,2)),
+                  ((0,), (1,), (2,)),
+                  ((2,), (1,0))]
+        instance.append_order_list(orders)
+        scores = pairwise_scores(instance)
+        assert scores[0][1] == 2
+        assert scores[1][0] == 0
+        assert scores[1][2] == 1
     
     def test_copeland_strict(self):
         instance = OrdinalInstance()
@@ -39,7 +50,7 @@ class TestAnalysis(TestCase):
         assert scores[3][0] == -2
         assert scores[1][2] == 0
 
-    def test_has_weak_condorcet(self):
+    def test_has_weak_condorcet_true(self):
         instance = OrdinalInstance()
         orders = [
             ((0,), (1,), (2,)),
@@ -47,6 +58,16 @@ class TestAnalysis(TestCase):
         ]
         instance.append_order_list(orders)
         assert has_condorcet(instance, weak_condorcet=True) is True
+
+    def test_has_weak_condorcet_false(self):
+        instance = OrdinalInstance()
+        orders = [
+            ((0,), (1,), (2,)),
+            ((2,), (0,), (1,)),
+            ((1,), (2,), (0,)),
+        ]
+        instance.append_order_list(orders)
+        assert has_condorcet(instance, weak_condorcet=True) is False
 
     def test_has_condorcet_false(self):
         instance = OrdinalInstance()
