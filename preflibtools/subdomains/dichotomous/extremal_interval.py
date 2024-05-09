0a2950213b2d17b847cd0dd65a79b991fab63849
dennistol01 <dennistol01@gmail.com>
2024-05-09 14:41:36 +0200

    Created Voter interval, still problems with VEI

--- preflibtools/subdomains/dichotomous/extremal_interval.py
+++ preflibtools/subdomains/dichotomous/extremal_interval.py
@@ -1,29 +1,73 @@
 import numpy as np
+from pq_trees import P
 
 def instance_to_matrix(instance, interval):
     alternatives = sorted(set().union(*instance))
     alternative_count = len(alternatives)
     voter_count = len(instance)
 
-    if interval == 'vei':
-        M = np.zeros((voter_count, alternative_count))
+    M = np.zeros((voter_count, alternative_count), dtype=int)
         
-        for idx, vote in enumerate(instance):
-            for alt in vote:
-                M[idx][alternatives.index(alt)] = 1
-    
-        return M
+    for idx, vote in enumerate(instance):
+        for alt in vote:
+            M[idx][alternatives.index(alt)] = 1
 
+    M = M.tolist()
+    print("M:", M)
+
+    if interval == 'vi':
+       return M, alternatives
+
+    elif interval == 'ci':
+        pass
+    elif interval == 'vei':
+        pass
     elif interval == 'cei':
         pass
-    else:
-        raise KeyError("Interval must be either 'vei' or 'cei' " )
 
+# Voter Interval (VI)
+def is_VI(instance):
+    # Comvert instance to matrix
+    M, alternatives = instance_to_matrix(instance, interval = 'vi')
+
+    # If input p is just the alternatives and not binary matrix (to list) it does work?
+    # but why
+    instances_list = [list(vote) for vote in instance]
+
+    # Make P tree and set contiguous for all alternatives to solve consecutive ones problem
+    p = P(M)
+    for alt in alternatives:
+        # If could not solve consecutive ones than False
+        if not p.set_contiguous(alt):
+            return False
+    
+    # Get order
+    order = p.ordering()
+    print("VI order:", order)
+    return True
 
 # Voter Extremal Interval (VEI)
 def is_VEI(instance):
-    M = instance_to_matrix(instance, interval = 'vei')
-    pass
+    '''
+    Started here but continued for VI in the meantime
+    '''
+    # Convert instance to matrix
+    M, alternatives = instance_to_matrix(instance, interval = 'vei')
+    # alternatives = sorted(set().union(*instance))
+
+    # Make P tree and set contiguous for all alternatives to solve consecutive ones problem
+    p = P(M)
+    print(p)
+    for idx, alt in enumerate(alternatives):  
+        p.set_contiguous(idx)
+        p.set_contiguous(alt)
+    
+    print(p)
+
+
+    ordering = p.ordering()
+
+    return p
 
 
 # Candidate Extremal Interval (CEI)
@@ -31,12 +75,16 @@ def is_CEI(instance):
     M = instance_to_matrix(instance, interval = 'cei')
     pass
 
+
+
 instance = [
-    {'A', 'B'},  # Voter 1 chooses alternatives A and B
-    {'C'},       # Voter 2 chooses alternative C
-    {'D', 'E'},  # Voter 3 chooses alternatives D and E
-    {'A', 'C'}   # Voter 4 chooses alternatives A and C
+    {'C', 'D'},
+    {'A', 'B', 'C'},
+    {'B', 'C'},
+    {'C'},
+    {'D'},
+    {'D'}
 ]
 
-res = instance_to_matrix(instance, 'vei')
-print(res)
+result = is_VI(instance)
+print("VI:", result)
