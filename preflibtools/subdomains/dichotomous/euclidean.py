ac27be3503a71a939b09a30ad25a47805bb16e94
dennistol01 <dennistol01@gmail.com>
2024-05-31 10:15:48 +0200

    Created file

---
+++ preflibtools/subdomains/dichotomous/euclidean.py
@@ -0,0 +1,24 @@
+from interval import is_CI
+
+
+# Check Dichotomous Euclidean
+def is_DE(instance):
+    res, _ = is_CI(instance)
+
+    if res is True:
+        return True, None
+    else:
+        return False, None
+
+# Check Possible Euclidean
+def is_PE(instance):
+    res, _ = is_CI(instance)
+
+    if res is True:
+        return True, None
+    else:
+        return False, None
+    
+
+def is_DUE(instance):
+    pass
