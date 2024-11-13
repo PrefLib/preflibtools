0bc0ca7cc36b80515e6ba4044c34f544a6a82cb7
Simon-Rey <reysimon@orange.fr>
2024-11-13 18:48:00 +0100

    Changed SC testing

--- tests/properties/subdomains/ordinal/test_single_crossing.py
+++ tests/properties/subdomains/ordinal/test_single_crossing.py
@@ -8,28 +8,32 @@ from preflibtools.instances.sampling import prefsampling_ordinal_wrapper
 from preflibtools.properties.subdomains.ordinal.singlecrossing import is_single_crossing, \
     is_single_crossing_conflict_sets
 
-import time
 
 class TestSingleCrossing(TestCase):
     def test_single_crossing_positive(self):
-
         for seed in range(50):
             params = {
-                "num_voters": 200,
+                "num_voters": 500,
                 "num_candidates": 30,
                 "seed": seed
             }
-            print(seed)
             vote_map = prefsampling_ordinal_wrapper(single_crossing, params)
-            print(vote_map)
             instance = OrdinalInstance()
             instance.append_vote_map(vote_map)
-            print(instance)
-
-            is_sc_conflict_sets = is_single_crossing_conflict_sets(instance)
-            assert is_sc_conflict_sets
-            print(is_sc_conflict_sets)
 
             is_sc, sc_orders = is_single_crossing(instance)
             assert is_sc
-            print(is_sc)
+
+    def test_single_crossing_conflict_sets_positive(self):
+        for seed in range(50):
+            params = {
+                "num_voters": 200,
+                "num_candidates": 10,
+                "seed": seed
+            }
+            vote_map = prefsampling_ordinal_wrapper(single_crossing, params)
+            instance = OrdinalInstance()
+            instance.append_vote_map(vote_map)
+
+            is_sc = is_single_crossing_conflict_sets(instance)
+            assert is_sc
